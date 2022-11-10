9b8c2684a3b2259d009a3a625d1c747e0fe2dc4b
shafik720 <shafikrasel5@gmail.com>
2022-11-10 09:29:11 +0600

    bootstrap and firebase added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h2>Hello</h2>
     </div>
   );
 }
