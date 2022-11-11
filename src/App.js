bbe67da397ceb8199777a7256a0666ca3967838d
shafik720 <shafikrasel5@gmail.com>
2022-11-11 23:28:47 +0600

    working on facebook log in

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import { useState } from 'react';
 
 function App() {
   const auth = getAuth(app);
+  const provider = new FacebookAuthProvider();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
