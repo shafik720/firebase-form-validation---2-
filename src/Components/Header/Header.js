5977cf2b98df74e67b45b53ff0843bbe67f1865d
shafik720 <shafikrasel5@gmail.com>
2022-11-12 22:23:29 +0600

    setting more route

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,11 @@ const Header = () => {
     return (
         <div className="headerDiv text-center">
             <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/shop">Shop</Link>
             <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
         </div>
     );
 };
