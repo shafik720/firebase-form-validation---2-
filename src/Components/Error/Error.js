0ccc084e251511d78677e9f41ef5c8b3374a8dc3
shafik720 <shafikrasel5@gmail.com>
2022-11-12 22:34:08 +0600

    setting more route

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -4,7 +4,7 @@ import './Error.css'
 const Error = () => {
     return (
         <div>
-            <h2 className="text-danger">Error 404 Not Found</h2>
+            <h2 style={{color:'red'}}>Error 404 Not Found</h2>
         </div>
     );
 };
