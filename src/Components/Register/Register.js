56578eaf5d2cd1682922d776213d490daf091a0e
shafik720 <shafikrasel5@gmail.com>
2022-11-12 22:50:13 +0600

    register page done

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form , Button } from 'react-bootstrap';
 import './Register.css'
 
 const Register = () => {
@@ -23,8 +24,8 @@ const Register = () => {
                                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                                     <Form.Check type="checkbox" label="Check me out" />
                                 </Form.Group>
-                                <Button variant="primary" type="submit">
-                                    Login
+                                <Button variant="success" type="submit">
+                                    Register
                                 </Button>
                             </Form>
                         </div>
