352f4decb8e374ddc926c7b4870602f708f78a8b
shafik720 <shafikrasel5@gmail.com>
2022-11-12 23:45:50 +0600

    working on hooks

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -3,8 +3,10 @@ import { useState } from "react";
 function useFirebase(){
     let [user, setUser] = useState({});
     function useGoogleAuth () {
-        
+        console.log('ok go');
     }
+
+    return {user, useGoogleAuth}
 }
 
 export default useFirebase;
