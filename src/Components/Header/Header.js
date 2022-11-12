9fec7d56faa6b3fe96234cbd911ea068f92ba816
shafik720 <shafikrasel5@gmail.com>
2022-11-12 20:40:46 +0600

    working on router

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <div className="headerDiv text-center">
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
         </div>
