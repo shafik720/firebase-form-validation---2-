70267b40a1dcaf7d2c07788b00e6801914e557b3
shafik720 <shafikrasel5@gmail.com>
2022-11-12 22:31:17 +0600

    setting more route

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ import { useState } from 'react';
 import Header from './Components/Header/Header';
 import { Route, Router, Routes } from 'react-router';
 import Login from './Components/Login/Login';
+import Home from './Home/Home';
 
 
 
 function App() { 
   
   return (
-    <div>
+    <div className="App">
       <Header></Header>
       <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="*" element={}></Route>
       </Routes>
     </div>
   );
