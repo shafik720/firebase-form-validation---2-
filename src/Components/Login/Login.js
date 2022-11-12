a6110f2af11bc3b8be71a8b3da051348d86dde36
shafik720 <shafikrasel5@gmail.com>
2022-11-12 22:47:11 +0600

    login page done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -13,11 +13,8 @@ const Login = () => {
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
                                     <Form.Label>Email address</Form.Label>
                                     <Form.Control type="email" placeholder="Enter email" />
-                                    <Form.Text className="text-muted">
-                                        We'll never share your email with anyone else.
-                                    </Form.Text>
                                 </Form.Group>
-
+                                
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
                                     <Form.Label>Password</Form.Label>
                                     <Form.Control type="password" placeholder="Password" />
@@ -26,7 +23,7 @@ const Login = () => {
                                     <Form.Check type="checkbox" label="Check me out" />
                                 </Form.Group>
                                 <Button variant="primary" type="submit">
-                                    Submit
+                                    Login
                                 </Button>
                             </Form>
                         </div>
