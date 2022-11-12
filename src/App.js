431c9ecfe460195acc5c08eb45ea84adbf7e95bd
shafik720 <shafikrasel5@gmail.com>
2022-11-12 09:27:09 +0600

    resetting all

--- src/App.js
+++ src/App.js
@@ -8,125 +8,12 @@ import { useState } from 'react';
 
 
 
-function App() {
-  const auth = getAuth(app);
-  const provider = new FacebookAuthProvider();
+function App() { 
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [registered, setRegistered] = useState(false);
-
-  function emailHandle(e) {
-    setEmail(e.target.value);
-  }
-  function passwordHandle(e) {
-    setPassword(e.target.value);
-  }
-  function checkBoxHandle(e) {
-    setRegistered(e.target.checked);
-  }
-  function verification() {
-    const auth = getAuth();
-    sendEmailVerification(auth.currentUser)
-      .then(() => {
-        console.log('Verification email sent');
-      });
-  }
-  function signUp(e) {
-    e.preventDefault();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        verification();
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setError(errorCode);
-        console.log(error)
-      });
-  }
-  function signIn(e) {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setError(errorCode);
-        console.log(errorCode);
-      });
-  }
-
-  function facebookSignIn(){
-    signInWithPopup(auth, provider)
-  .then((result) => {
-    const user = result.user;
-    const credential = FacebookAuthProvider.credentialFromResult(result);
-    const accessToken = credential.accessToken;
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setError(errorCode);
-    console.log(errorCode);
-  });
-  }
+  
   return (
     <div>
-      <div className="container my-5">
-        <div className="row">
-          <div className="col-lg-6 mx-auto">
-            <Form>
-              <h2 className="text-center text-primary"> {registered ? 'Log In' : 'Sign Up'} </h2>
-
-              {registered && <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>User Name :</Form.Label>
-                <Form.Control onBlur={emailHandle} type="email" placeholder="Username" />
-              </Form.Group>}
-
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control onBlur={emailHandle} type="email" placeholder="Enter email" required />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control onBlur={passwordHandle} type="password" placeholder="Password" />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check onChange={checkBoxHandle} type="checkbox" label="Already a Member" />
-              </Form.Group>
-              {/* error message  */}
-              <h4 className="text-danger">{error}</h4>
-              {registered ?
-                <Button onClick={signIn} variant="primary" type="submit">
-                  Log In
-                </Button>
-                :
-                <Button onClick={signUp} variant="primary" type="submit">
-                  Sign Up
-              </Button>
-              }
-
-            </Form>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-6 mx-auto my-5">
-            <div className="">
-              <button onClick={facebookSignIn} >Connect With Facebook</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <h2>Refreshing all</h2>
     </div>
   );
 }
