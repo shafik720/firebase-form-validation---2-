f2fdd61d112f8d62797d33cb09ef78d75b8634da
shafik720 <shafikrasel5@gmail.com>
2022-11-12 22:49:02 +0600

    working on register page

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -4,7 +4,34 @@ import './Register.css'
 const Register = () => {
     return (
         <div>
-            <h2>Register Page</h2>
+            <div className="login-div">
+            <h2 className="text-center">Register</h2>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 mx-auto">
+                        <div className="">
+                            <Form>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                </Form.Group>
+                                
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Login
+                                </Button>
+                            </Form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
