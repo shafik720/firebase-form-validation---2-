9bc8bd1a41aeadf533ec0a624a35e828b4d02223
shafik720 <shafikrasel5@gmail.com>
2022-11-13 20:33:58 +0600

    user is being sent to homepage

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import useFirebase from '../hooks/useFirebase';
 import './Home.css'
 
 const Home = () => {
+    const{user} = useFirebase();
     return (
         <div>
             <h2 className="text-center">Home is Where the Heart is !</h2>
+            
         </div>
     );
 };
