c4adf5a2cf31d63d00e5d93fd0b76e936e6b45d7
shafik720 <shafikrasel5@gmail.com>
2022-11-13 15:33:19 +0600

    trying from another pc

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -10,14 +10,10 @@ function useFirebase() {
 
         signInWithPopup(auth, provider)
             .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
                 const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
-                // The signed-in user info.
                 const user = result.user;
-                console.log(user);
             }).catch((error) => {
-                // Handle Errors here.
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 console.log(errorCode);
