8bb4ad090f081a6e0971d4a6f7f283ad5cbb16d4
shafik720 <shafikrasel5@gmail.com>
2022-11-13 18:33:46 +0600

    trying from another pc

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Header.css'
 
 const Header = () => {
+    let {user} = useFirebase();
     return (
         <div className="headerDiv text-center">
             <Link to="/">Home</Link>
