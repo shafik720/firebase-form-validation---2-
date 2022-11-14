483f8b8d490f4c52f108d231e855f4c02d04735d
shafik720 <shafikrasel5@gmail.com>
2022-11-14 12:27:19 +0600

    trying rotected route

--- src/Components/RequireAuth/RequireAuth.js
+++ src/Components/RequireAuth/RequireAuth.js
@@ -1,6 +1,12 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import app from '../../firebase.init';
 
 const RequireAuth = ({ children }) => {
+    let auth = getAuth(app);
+    let location = useLocation();
+    
     return (
         <div>
             
