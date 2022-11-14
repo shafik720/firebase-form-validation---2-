7c0d09aeffa17774448914a4cf39d3f4aeab575c
shafik720 <shafikrasel5@gmail.com>
2022-11-14 11:54:16 +0600

    trying rotected route

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
+            <Link to='/orders'>Orders</Link>
             <Link to="/shop">Shop</Link>
             <span>{
                     user?.uid ? `${user.displayName}` : ''
