0ba7143fc793a73aaa89f2d5c4ea84e32f3b7ebc
shafik720 <shafikrasel5@gmail.com>
2022-11-14 13:35:46 +0600

    adding steps for remembering next time on new project

---
+++ src/notes_step.js
@@ -0,0 +1,7 @@
+/* 
+    1. Create a new Project in console.google.com
+    2. npm install firebase
+    3. create.firebase.init.js in vsCode and import getAuth to export Auth.
+    4. firebase settings > authentication > enable email & password auth
+    5.
+*/
